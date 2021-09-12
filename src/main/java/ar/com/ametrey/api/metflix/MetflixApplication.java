6ff7647b91ccd17be9a7056a54576d1eb12c2c2c
ametrey <aetcheverryrey@gmail.com>
2021-09-12 20:28:08 -0300

    first commit

---
+++ src/main/java/ar/com/ametrey/api/metflix/MetflixApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ametrey.api.metflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ametrey/api/metflix/MetflixApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ametrey.api.metflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
